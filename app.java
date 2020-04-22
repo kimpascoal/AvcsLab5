d64f26214f3b8bb11124d8fc4b186a361256cc9d
kimpascoal <kimpascoal09@gmail.com>
2020-04-23 00:03:54 +0400

    code file app added

---
+++ app.java
@@ -0,0 +1,11 @@
+mport React from 'react';
+import Task from "./components/Task";
+
+export default function App(props) {
+    return (
+        // JSX - javascript and XML
+        <>
+            <Task />
+        </>
+    )
+}
